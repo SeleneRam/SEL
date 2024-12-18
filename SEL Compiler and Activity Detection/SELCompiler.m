fa4e0acbb27007135334455fa3bb57c8456f0967
Selene Ramirez-Rosales <165323970+SeleneRam@users.noreply.github.com>
2024-12-18 17:23:33 -0600

    Add files via upload

--- SEL Compiler and Activity Detection/SELCompiler.m
+++ SEL Compiler and Activity Detection/SELCompiler.m
@@ -1,20 +1,22 @@
+% -*- octave -*-
+clc;
+clear all;
+close all;
 
-clc all
-clear all
-close all
 pkg load video;
 pkg load image;
 
 prompt = {'SEL SCRIPT NAME:'};
 dlgtitle = 'S E L';
-fieldsize = [1 40];
-inputdlgdefinput={''};
-answer = inputdlg(prompt,dlgtitle,fieldsize,inputdlgdefinput);
+fieldsize = [1 15];
+inputdlgdefinput = {''};
+answer = inputdlg(prompt, dlgtitle, fieldsize, inputdlgdefinput);
+duracion = 0;
+totalFrames = 0;
+val = answer{1, 1};
+variable = true;
 
-val=answer{1,1};
-variable=true;
 while (variable)
-
     n = 1;
     i = 0;
 
@@ -23,7 +25,7 @@ while (variable)
     video = VideoReader(videoname);
     matriz = AFD;
     actual = double(rgb2gray(readFrame(video)));
-    numIntrucciones = (length(matriz));
+    numIntrucciones = length(matriz);
     n = 1;
     afd2 = {};
     actual = double(rgb2gray(readFrame(video)));
@@ -33,34 +35,39 @@ while (variable)
     ORIGINAL = readFrame(video);
     actual = double(rgb2gray(readFrame(video)));
     actual = imresize(actual, .30);
-     disp("   W O R K I N G. . . 0%");
-   while (hasFrame(video))
 
+    disp("   W O R K I N G. . . 0%");
+
+    h = waitbar(0, 'W O R K I N G. . .');
+    total = video.NumberOfFrames;
+    duracion = video.Duration;
+    totalFrames = video.NumberOfFrames;
+
+    while (hasFrame(video))
         [video, temporal, actual, f] = analisisAFD(video, estados, matriz, arrayEdos, actual, f, i);
         videoP = video;
         afd2{n} = temporal;
         n = n + 1;
 
-        if (mod(n,100)==1)
-      porcentaje=  n*100/video.NumberOfFrames;
-          disp(["   W O R K I N G. . . "  int2str(porcentaje) '%']);
+        if (mod(n, 50) == 1)
+            porcentaje = n * 100 / total;
+            disp(["   W O R K I N G. . . " int2str(porcentaje) '%']);
+            waitbar(n / total, h, sprintf('W O R K I N G. . . %d%%', round((n / total) * 100)));
         end
-
-
     end
 
+    close(h);
+
     afdEnd = [];
-
     afd2 = afd2(2:end);
-     for xx=1:numIntrucciones
-        frames=parseAFD2(AFD{xx},afd2);
-        frames=[frames 0];
-         afdEnd{xx}=frames;
-     end
 
-     [rr, rr2]=analisisEtiquetas2(afdEnd,numIntrucciones, AFDEtiqueta, AFDnombres);
-    %
-    %
-variable=false;
+    for xx = 1:numIntrucciones
+        frames = parseAFD2(AFD{xx}, afd2);
+        frames = [frames 0];
+        afdEnd{xx} = frames;
+    end
 
+    [r, r2, lista] = analisisEtiquetas2(afdEnd, numIntrucciones, AFDEtiqueta, AFDnombres, duracion, totalFrames);
+    variable = false;
 end
+
--- SEL Compiler and Activity Detection/analisisEtiquetas2.m
+++ SEL Compiler and Activity Detection/analisisEtiquetas2.m
@@ -1,4 +1,5 @@
-function [r,r2]=analisisEtiquetas2(afdEnd,numIntrucciones, AFDEtiqueta, AFDnombres)
+function [r,r2,lista]=analisisEtiquetas2(afdEnd,numIntrucciones, AFDEtiqueta, AFDnombres, duracion, totalFrames )
+%aqui
 aa=ones(1,numIntrucciones);
 aaa=aa;
 x=1;
@@ -7,10 +8,15 @@ r2=[];
 r3=[];
 valor=[];
 numero=aa;
+fps=totalFrames/duracion;
+lista = cell(1,numIntrucciones)
 while(x~= -1  )
 for j=1:numIntrucciones
     for i=aa(j):length(afdEnd{j})
         ex=afdEnd{j}(i);
+        frame_duration = duracion / totalFrames;
+        time_of_frame = (ex - 1) * frame_duration;
+        time_string = sprintf('%.2f sec', time_of_frame);
         if (i<=length(afdEnd{j})-1 && aaa(j)~=-1)
         if (ex+1~=afdEnd{j}(i+1) )
          r=[r;AFDnombres(find(AFDEtiqueta==j))];
@@ -18,7 +24,8 @@ for j=1:numIntrucciones
              r2=[r2;j];
              r3=[r3;ex];
              numero(j)=numero(j)+1;
-               % disp([AFDnombres{j} '= ' int2str(i)]);
+               %disp([AFDnombres{j} '= 1-' int2str(i)]);
+              lista{j} = [lista{j},",", int2str(time_string)];
              break;
         end
         end
@@ -29,7 +36,8 @@ for j=1:numIntrucciones
              numero(j)=numero(j)+1;
              r2=[r2;j];
               r3=[r3;ex];
-                % disp([AFDnombres{j} '= ' int2str(i)]);
+                 %disp([AFDnombres{j} '= 2-' int2str(i)]);
+                   lista{j} = [lista{j},",¡", int2str(time_string)];
              break;
          end
         if (aa(j)==length(afdEnd{j}) && aaa(j)~=-1)
@@ -39,7 +47,8 @@ for j=1:numIntrucciones
             numero(j)=numero(j)+1;
             r2=[r2;j];
              r3=[r3;ex];
-             %disp([AFDnombres{j} '= ' int2str(i)]);
+            % disp([AFDnombres{j} '= 3--' int2str(i)]);
+               % lista{j} = [lista{j},",*", int2str(time_string)];
              break
          end
     end
@@ -69,18 +78,27 @@ for y=1:length(r3)
 end
 
 end
-disp("Activity count:");
-itemlist={};
-for g=1:length(aa)
-    s=r2==g;
-    resultado=sum(s)-1;
-    disp(['      ' AFDnombres{g} '=' int2str(resultado)]);
-   %itemlist = {itemlist,['      ' AFDnombres{g} '=' int2str(resultado)]};
-   itemlist = [itemlist,['      ' AFDnombres{g} '=' int2str(resultado)]];
-end
-%itemlist = itemlist(1:end);
 
- s = listdlg ("ListString", itemlist,"SelectionMode","single");
+VAR = 0;
+disp("Activity count:");
+itemlist = {};
+
+
+for g = 1:length(aa)
+    s = r2 == g;
+    VAR = VAR + 25;
+    resultado = sum(s) - 1;
+    disp(['      ' AFDnombres{g} '=' int2str(resultado), lista{g}]);
+
+
+    item_text = ['      ' AFDnombres{g} ' = **' int2str(resultado) '**    '  lista{g}];
+    itemlist = [itemlist, item_text];
+end
+
+
+list_size = [200, max(150, VAR)];
+s = listdlg("ListString", itemlist, "SelectionMode", "single", "ListSize", list_size);
+
 
 
 
